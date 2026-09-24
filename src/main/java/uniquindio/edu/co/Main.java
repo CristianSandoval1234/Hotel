bc670dfff292d10781b115fb0688a7d918d1a018
CristianSandoval1234 <cristiana.sandovalf@uqvirtual.edu.co>
2026-09-24 15:20:36 -0500

    cambie nombre

--- src/main/java/uniquindio/edu/co/Main.java
+++ src/main/java/uniquindio/edu/co/Main.java
@@ -4,7 +4,7 @@ package uniquindio.edu.co;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main(String[] args) {
-        System.out.println("Hola Mundo soy Cristian alberto ");
+        System.out.println("Hola Mundo soy Cristian ");
         }
     }
 
